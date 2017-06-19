d5b44e9e2c84faf910c44ec765bcd79f06c63974
Jeff Dupont <jdupont@brightcove.com>
2017-06-19 09:31:57 -0700

    updated the source mapped volume for the build script

--- build-snap.sh
+++ build-snap.sh
@@ -23,7 +23,7 @@ then
 fi
 
 CID=$(docker run -d buildertools/dynamodb-local)
-BID=$(docker run -d -v "$(pwd)"/"$1":/usr/local/db-script/"$1" --net container:$CID buildertools/dynamodb-snap-builder ./"$1")
+BID=$(docker run -d -v "$1":/usr/local/db-script/"$1" --net container:$CID buildertools/dynamodb-snap-builder ./"$1")
 docker wait $BID > /dev/null
 echo "Build complete using: $1"
 docker stop $CID > /dev/null
